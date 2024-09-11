38647da066c34b1c470724d4b162913e157e1870
crosscap <wangliankun2018@outlook.com>
2024-09-11 21:13:22 +0800

    finish l16 Segmentation

--- homework/vm-segmentation/segmentation.py
+++ homework/vm-segmentation/segmentation.py
@@ -1,10 +1,11 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 from __future__ import print_function
 import sys
 from optparse import OptionParser
 import random
 import math
+
 
 # to make Python2 and Python3 act the same -- how dumb
 def random_seed(seed):
@@ -13,6 +14,7 @@ def random_seed(seed):
     except:
         random.seed(seed)
     return
+
 
 def convert(size):
     length = len(size)
@@ -30,6 +32,7 @@ def convert(size):
         nsize = int(size)
     return nsize
 
+
 def abort_if(condition, message):
     if condition:
         print('Error:', message)
@@ -41,16 +44,26 @@ def abort_if(condition, message):
 # main program
 #
 parser = OptionParser()
-parser.add_option("-s", "--seed", default=0, help="the random seed", action="store", type="int", dest="seed")
-parser.add_option("-A", "--addresses", default="-1", help="a set of comma-separated pages to access; -1 means randomly generate", action="store", type="string", dest="addresses")
-parser.add_option("-a", "--asize", default="1k", help="address space size (e.g., 16, 64k, 32m, 1g)", action="store", type="string", dest="asize")
-parser.add_option("-p", "--physmem", default="16k", help="physical memory size (e.g., 16, 64k, 32m, 1g)", action="store", type="string", dest="psize")
-parser.add_option("-n", "--numaddrs", default=5, help="number of virtual addresses to generate", action="store", type="int", dest="num")
-parser.add_option("-b", "--b0", default="-1", help="value of segment 0 base register", action="store", type="string", dest="base0")
-parser.add_option("-l", "--l0", default="-1", help="value of segment 0 limit register", action="store", type="string", dest="len0")
-parser.add_option("-B", "--b1", default="-1", help="value of segment 1 base register", action="store", type="string", dest="base1")
-parser.add_option("-L", "--l1", default="-1", help="value of segment 1 limit register", action="store", type="string", dest="len1")
-parser.add_option("-c", help="compute answers for me", action="store_true", default=False, dest="solve")
+parser.add_option("-s", "--seed", default=0, help="the random seed",
+                  action="store", type="int", dest="seed")
+parser.add_option("-A", "--addresses", default="-1", help="a set of comma-separated pages to access; -1 means randomly generate",
+                  action="store", type="string", dest="addresses")
+parser.add_option("-a", "--asize", default="1k", help="address space size (e.g., 16, 64k, 32m, 1g)",
+                  action="store", type="string", dest="asize")
+parser.add_option("-p", "--physmem", default="16k",
+                  help="physical memory size (e.g., 16, 64k, 32m, 1g)", action="store", type="string", dest="psize")
+parser.add_option("-n", "--numaddrs", default=5,
+                  help="number of virtual addresses to generate", action="store", type="int", dest="num")
+parser.add_option("-b", "--b0", default="-1", help="value of segment 0 base register",
+                  action="store", type="string", dest="base0")
+parser.add_option("-l", "--l0", default="-1", help="value of segment 0 limit register",
+                  action="store", type="string", dest="len0")
+parser.add_option("-B", "--b1", default="-1", help="value of segment 1 base register",
+                  action="store", type="string", dest="base1")
+parser.add_option("-L", "--l1", default="-1", help="value of segment 1 limit register",
+                  action="store", type="string", dest="len1")
+parser.add_option("-c", help="compute answers for me",
+                  action="store_true", default=False, dest="solve")
 
 (options, args) = parser.parse_args()
 
@@ -66,7 +79,8 @@ addresses = str(options.addresses)
 
 abort_if(psize <= 4, 'must specify a bigger physical memory size')
 abort_if(asize == 0, 'must specify a non-zero address-space size')
-abort_if(psize <= asize, 'physical memory size > address space size (for this simulation)')
+abort_if(psize <= asize,
+         'physical memory size > address space size (for this simulation)')
 
 #
 # need to generate base, bounds for segment registers
@@ -84,7 +98,8 @@ if len1 == -1:
 
 if base0 == -1 or base1 == -1:
     # this restriction just makes it easier to place randomly-placed segments
-    abort_if(psize <= 2 * asize, 'physical memory must be 2x GREATER than address space size (if randomly generating base registers)')
+    abort_if(psize <= 2 * asize,
+             'physical memory must be 2x GREATER than address space size (if randomly generating base registers)')
 
 # if randomly generate base, have to find room for them
 if base0 == -1:
@@ -108,7 +123,7 @@ else:
 
 abort_if(psize < base0 + len0 - 1, 'seg0 is not in physical memory')
 abort_if(psize < base1, 'seg1 is not in physical memory')
-    
+
 abort_if(len0 > asize/2.0, 'length0 register is too large for this address space')
 abort_if(len1 > asize/2.0, 'length1 register is too large for this address space')
 
@@ -117,13 +132,15 @@ print('')
 print('  Segment 0 base  (grows positive) : 0x%08x (decimal %d)' % (base0, base0))
 print('  Segment 0 limit                  : %d' % (len0))
 print('')
-print('  Segment 1 base  (grows negative) : 0x%08x (decimal %d)' % (base1+len1, base1+len1))
+print('  Segment 1 base  (grows negative) : 0x%08x (decimal %d)' %
+      (base1+len1, base1+len1))
 print('  Segment 1 limit                  : %d' % (len1))
 print('')
 
 nbase1 = base1 + len1
 
-abort_if((len0 + base0) > base1 and (base1 > base0), 'segments overlap in physical memory')
+abort_if((len0 + base0) > base1 and (base1 > base0),
+         'segments overlap in physical memory')
 
 addrList = []
 if addresses == '-1':
@@ -142,10 +159,12 @@ i = 0
 for vstr in addrList:
     vaddr = int(vstr)
     if vaddr < 0 or vaddr >= asize:
-        print('Error: virtual address %d cannot be generated in an address space of size %d' % (vaddr, asize))
+        print('Error: virtual address %d cannot be generated in an address space of size %d' % (
+            vaddr, asize))
         exit(1)
     if options.solve == False:
-        print('  VA %2d: 0x%08x (decimal: %4d) --> PA or segmentation violation?' % (i, vaddr, vaddr))
+        print('  VA %2d: 0x%08x (decimal: %4d) --> PA or segmentation violation?' %
+              (i, vaddr, vaddr))
     else:
         paddr = 0
         if (vaddr >= (asize / 2)):
@@ -153,16 +172,20 @@ for vstr in addrList:
             #  [base1+len1]  [negative offset]
             paddr = nbase1 + (vaddr - asize)
             if paddr < base1:
-                print('  VA %2d: 0x%08x (decimal: %4d) --> SEGMENTATION VIOLATION (SEG1)' % (i, vaddr, vaddr))
+                print(
+                    '  VA %2d: 0x%08x (decimal: %4d) --> SEGMENTATION VIOLATION (SEG1)' % (i, vaddr, vaddr))
             else:
-                print('  VA %2d: 0x%08x (decimal: %4d) --> VALID in SEG1: 0x%08x (decimal: %4d)' % (i, vaddr, vaddr, paddr, paddr))
+                print('  VA %2d: 0x%08x (decimal: %4d) --> VALID in SEG1: 0x%08x (decimal: %4d)' %
+                      (i, vaddr, vaddr, paddr, paddr))
         else:
             # seg 0
             if (vaddr >= len0):
-                print('  VA %2d: 0x%08x (decimal: %4d) --> SEGMENTATION VIOLATION (SEG0)' % (i, vaddr, vaddr))
+                print(
+                    '  VA %2d: 0x%08x (decimal: %4d) --> SEGMENTATION VIOLATION (SEG0)' % (i, vaddr, vaddr))
             else:
                 paddr = vaddr + base0
-                print('  VA %2d: 0x%08x (decimal: %4d) --> VALID in SEG0: 0x%08x (decimal: %4d)' % (i, vaddr, vaddr, paddr, paddr))
+                print('  VA %2d: 0x%08x (decimal: %4d) --> VALID in SEG0: 0x%08x (decimal: %4d)' %
+                      (i, vaddr, vaddr, paddr, paddr))
     i += 1
 
 print('')
@@ -176,8 +199,3 @@ if options.solve == False:
     print('given to you grow in different directions, depending on the segment, i.e., segment 0')
     print('grows in the positive direction, whereas segment 1 in the negative. ')
     print('')
-
-
-
-
-
