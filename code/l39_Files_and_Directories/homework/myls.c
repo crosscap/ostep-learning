b2ce66aab94a14d61d28d5a67adade53ee71511d
crosscap <wangliankun2018@outlook.com>
2024-10-11 16:36:13 +0800

    finish q2 myls of l39

--- code/l39_Files_and_Directories/homework/myls.c
+++ code/l39_Files_and_Directories/homework/myls.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <dirent.h>
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -22,17 +23,44 @@ int main(int argc, const char *argv[])
 			strcpy(folder_name, argv[1]);
 		}
 	} else if (argc == 3) {
+		for (int i = 1; i < argc; i++) {
+			if (strcmp(argv[i], "-l") == 0) {
+				l_flag = 1;
+			} else {
+				strcpy(folder_name, argv[i]);
+			}
+		}
 	} else {
 		fprintf(stderr, "please enter right argument\n");
 		exit(1);
 	}
 
 	struct stat fileinfo;
+	struct dirent *d;
+	DIR *dp = opendir(folder_name);
+	assert(dp != NULL);
 
-	int rc = stat(filename, &fileinfo);
-	assert(rc == 0);
+	switch (l_flag) {
+	case 0:
+		printf("name\n");
+		while ((d = readdir(dp)) != NULL) {
+			printf("%s\n", d->d_name);
+		}
+		break;
 
-
+	case 1:
+		printf("file mode\thardlink count\tuser id\tsize\tname\n");
+		while ((d = readdir(dp)) != NULL) {
+			stat(d->d_name, &fileinfo);
+			printf("%x\t%ld\t%d\t%ld\t%s\n",
+				fileinfo.st_mode,
+				fileinfo.st_nlink,
+				fileinfo.st_uid,
+				fileinfo.st_size,
+				d->d_name);
+		}
+		break;
+	}
 
 	return 0;
 }
